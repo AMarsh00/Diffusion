ddc4ff215637fe936f3fd2eb305db530ac62ee5c
AMarsh00 <56977848+AMarsh00@users.noreply.github.com>
2026-03-07 18:57:57 -0700

    Update Geodesics.py

--- Geodesics.py
+++ Geodesics.py
@@ -1,7 +1,7 @@
 """
 Geodesics.py
 Alexander Marsh
-27 January 2026
+7 March 2026
 
 Computes geodesics between random generations of the CelebA-HQ diffusion model with our metric for various values of theta.
 """
